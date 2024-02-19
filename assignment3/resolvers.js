9117c18f75d9f2c090c3b5d5297fb256cabef8d9
bhavin79 <bhavinpatanwadia99@gmail.com>
2024-02-19 15:59:21 -0500

    lab3 set up complete

---
+++ assignment3/resolvers.js
@@ -0,0 +1,9 @@
+import {GraphQLError} from 'graphql';
+import { albums, artists, recordcompanies } from './config/mongoCollection.js';
+
+export const resolvers = {
+    Query:{
+        
+    }
+
+}

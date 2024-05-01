b750382bd182ce1cf6980e9e0c9f656b13a6d0f5
bhavin79 <bhavinpatanwadia99@gmail.com>
2024-04-30 22:53:14 -0400

    Final Commit

--- assignment7/src/app/page.js
+++ assignment7/src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="homeContainer">
         <p>
-        &apos; Welcome to SpaceX API Master, a user-friendly interface built upon the SpaceX API. Our platform offers comprehensive data across various categories such as Rockets, Launches, LaunchPads, Ships, Cores, and Payloads. We've designed the interface to streamline information, ensuring ease of access. Moreover, all pages support pagination, allowing seamless navigation, while interconnected data across categories enhances the user experience.
+        &apos; Welcome to SpaceX API Master, a user-friendly interface built upon the SpaceX API. Our platform offers comprehensive data across various categories such as Rockets, Launches, LaunchPads, Ships, Cores, and Payloads. We&rsquo;ve designed the interface to streamline information, ensuring ease of access. Moreover, all pages support pagination, allowing seamless navigation, while interconnected data across categories enhances the user experience.
         &apos;</p>
         <p> -Bhavin Patanwadia</p>
     </div>
